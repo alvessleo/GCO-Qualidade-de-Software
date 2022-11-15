840278008976b5bdd0ee0a803b7a1ca1f652308e
Leonardo Alves <90728273+alvessleo@users.noreply.github.com>
2022-11-14 23:49:48 -0300

    amanha continuo

--- js/minha_empresa.js
+++ js/minha_empresa.js
@@ -1,10 +1,6 @@
 // Gerenciando o form
 const form = document.querySelector("div.form-funcionario");
 
-const teste = document.getElementById("add-func");
-document.getElementById("add-func").style.color="black";
-
-teste.addEventListener("click", () => {
+document.getElementById("add-func").addEventListener("click", () => {
     form.style.display="flex";
-    console.log("oi")
 });
